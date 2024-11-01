da8bfa461e24629eabff1be1e6118d4e7c13333f
lpp <geolifestudy@gmail.com>
2024-11-01 17:24:46 +0800

    uno css 修改

--- miniComponent/animation/StateLoading.vue
+++ miniComponent/animation/StateLoading.vue
@@ -1,5 +1,5 @@
 <template>
-    <div full-fex position-center bg-opacity-10 v-if="isloading">
+    <div full-flex position-center bg-opacity-10 v-if="isloading">
         <svg aria-hidden="true" inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600
             viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
