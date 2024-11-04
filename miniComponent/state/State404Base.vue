e9b853c0ed0c0cbb14dd2b5d6e4d28cf3e7bc8d0
lpp <geolifestudy@gmail.com>
2024-11-04 17:30:05 +0800

    文件管理

---
+++ miniComponent/state/State404Base.vue
@@ -0,0 +1,26 @@
+<template>
+    <section w-full h-full flex bg-deep-2>
+  
+      <div mx-auto text-center class="mt-10%">
+        <h1 mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500>
+          404
+        </h1>
+        <p mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white>
+          Something's missing.
+        </p>
+        <p mb-4 text-lg font-light text-gray-500 dark:text-gray-400>
+          Sorry, we can't find that page. You'll find lots to explore on the home page.
+        </p>
+        <a href="/" bg-deep-3 inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none
+          focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4
+         >Back to Homepage</a>
+      </div>
+  
+    </section>
+  </template>
+  <script setup lang="ts">
+  // import router from '@/router';
+  // const goHome = () => {
+  //   router.push('/')
+  // }
+  </script>
---
+++ miniComponent/state/StateLoading.vue
@@ -0,0 +1,24 @@
+<template>
+  <section w-full h-full flex bg-deep-1>
+
+    <div mx-auto text-center class="mt-10%">
+      <h1 mb-4 text-5xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500>
+        loading...
+      </h1>
+      <p mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white>
+        Something's loading.
+      </p>
+
+      <a href="/" bg-deep-3 inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none
+        focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4
+       >Back to Homepage</a>
+    </div>
+
+  </section>
+</template>
+  <script setup lang="ts">
+  // import router from '@/router';
+  // const goHome = () => {
+  //   router.push('/')
+  // }
+  </script>
